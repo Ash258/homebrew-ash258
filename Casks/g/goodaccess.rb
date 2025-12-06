d79e8eccdfa09f4525a0ad3f362e27c2f69eba92
Jakub Čábera <cabera.jakub@gmail.com>
2025-12-06 23:20:04 +0100

    feat(Goodaccess): Add 4.7.0

---
+++ Casks/g/goodaccess.rb
@@ -0,0 +1,26 @@
+cask "goodaccess" do
+  version "4.7.0"
+  sha256 "fa74bf46c05b40f2ff6715844331de05f93b0253dd07b9852cae7b4762d6beb2"
+
+  url "https://goodaccess-storage.b-cdn.net/applications/prod/macOS/GoodAccess_#{version}.dmg",
+      verified: "goodaccess-storage.b-cdn.net/"
+  name "GoodAccess"
+  desc "Secure Remote Access VPN"
+  homepage "https://www.goodaccess.com/"
+
+  livecheck do
+    url "https://link.goodaccess.com/download-mac-link"
+    strategy :header_match
+    regex(%r{/GoodAccess[._-]v?([\d.]+)\.dmg}i)
+  end
+
+  depends_on macos: ">= :tahoe"
+
+  app "GoodAccess.app"
+
+  zap trash: [
+    "~/Library/Application Support/GoodAccess",
+    "~/Library/Caches/com.samohyb.GoodAccessMac",
+    "~/Library/Caches/SentryCrash/GoodAccess"
+  ]
+end
